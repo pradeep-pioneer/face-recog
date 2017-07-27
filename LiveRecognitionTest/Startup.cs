e04046c358b998fea13096f6c71494c9b85d1f45
Pradeep Singh <pradeep@veridic.in>
2017-07-27 12:54:32 +0530

    Add project files.

---
+++ LiveRecognitionTest/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LiveRecognitionTest.Startup))]
+namespace LiveRecognitionTest
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
